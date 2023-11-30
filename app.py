8280ae3d70185117d7f0b5e477c1b53a67446b66
ValerKoll <valerio.uk@gmail.com>
2023-11-30 11:11:27 +0000

    diagram added

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from lib.book_repository import BookRepository
 # Connect to the database
 connection = DatabaseConnection()
 connection.connect()
+print(connection.DATABASE_NAME)
+
+# Seed with some seed data
+connection.seed("seeds/book_store.sql")
 
 book_repo = BookRepository(connection)
 books = book_repo.all()
